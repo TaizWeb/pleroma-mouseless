d10b3b2ca5bb6d40e3ca82c641824a3b93f45197
taiz <tyler.walker.student@gmail.com>
2020-02-09 17:41:28 -0500

    Add swapping between timeline/notifcations

--- keybinds.js
+++ keybinds.js
@@ -4,6 +4,11 @@
 
 // Initializing timeline and selected post
 var timeline = document.getElementsByClassName("timeline");
+// The container of each notification, each index is a notif
+var notifications = document.getElementsByClassName("notifications")[0].children[0].children[1].children;
+var oldTimelineNumber;
+var oldNotificationsNumber = 0;
+var selectedColumn = "timeline";
 selectedPostNumber = 2;
 selectedPost = timeline[selectedPostNumber];
 selectedPost.style.border = "1px solid red";
@@ -15,12 +20,37 @@ window.addEventListener("keydown", keyDownHandler);
 function moveSelection(direction) {
 	let increment = (direction == "up") ? -1 : 1;
 	// Break out of the function if at top post
-	if (selectedPostNumber <= 2 && increment == -1) return;
+	if (selectedPostNumber <= 2 && increment == -1 && selectedColumn == "timeline") return;
+	if (selectedPostNumber <= 0 && increment == -1 && selectedColumn == "notifications") return;
+
 	selectedPost.style.border = "none";
 	selectedPostNumber += increment;
-	selectedPost = timeline[selectedPostNumber];
+	if (selectedColumn == "timeline")
+		selectedPost = timeline[selectedPostNumber];
+	else if (selectedColumn == "notifications")
+		selectedPost = notifications[selectedPostNumber];
 	selectedPost.style.border = "1px solid red";
 	selectedPost.scrollIntoView({behavior: 'auto', block: 'center', inline: 'center'});
+}
+
+// gotoNotifications: Moves user to notifications section
+function gotoNotifications() {
+	selectedColumn = "notifications";
+	selectedPost.style.border = "none";
+	oldTimelineNumber = selectedPostNumber;
+	selectedPostNumber = oldNotificationsNumber;
+	selectedPost = notifications[selectedPostNumber];
+	selectedPost.style.border = "1px solid red";
+}
+
+// gotoTimeline: Moves user to timeline section
+function gotoTimeline() {
+	selectedColumn = "timeline";
+	selectedPost.style.border = "none";
+	oldNotificationsNumber = selectedPostNumber;
+	selectedPostNumber = oldTimelineNumber;
+	selectedPost = timeline[selectedPostNumber];
+	selectedPost.style.border = "1px solid red";
 }
 
 // interactPost: Does an action on the selected post
@@ -60,6 +90,14 @@ function keyDownHandler(key) {
 		case "ArrowUp":
 		case "k":
 			moveSelection("up");
+			break;
+		case "ArrowLeft":
+		case "h":
+			gotoNotifications();
+			break;
+		case "ArrowRight":
+		case "l":
+			gotoTimeline();
 			break;
 		// Refresh/Read
 		case "r":
